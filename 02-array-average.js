209502478df59e51d23a3e646bc7fe340c57026a
Primes21 <eliemabiala009@gmail.com>
2025-11-06 18:27:33 +0100

    Résolution de l'exercice sur les notes

--- 02-array-average.js
+++ 02-array-average.js
@@ -2,10 +2,23 @@
 // Écrire une fonction averageNote avec `notes` comme parametre qui effectue les opérations suivantes :
 // - Calcule la moyenne des notes
 // - retourne un message "Réussi" si la moyenne est >= 10, sinon "Échoué"
+const notes = [10, 10, 10, 10];
 
 function averageNote(notes) {
-	
+	let somme = 0;
+	for (let i = 0; i < notes.length; i++) {
+		somme += notes[i];
+	}
+	let moyenne = somme / notes.length;
+
+	if(moyenne >= 10) {
+		return 'Réussi'
+	}else {
+		return 'Echoué'
+	}
 }
+
+console.log(averageNote(notes));
 
 module.exports = {
 	averageNote,
